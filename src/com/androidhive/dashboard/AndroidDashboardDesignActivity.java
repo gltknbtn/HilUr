6b336e8cc5286c2745b8cc9ae33dd3a1608b38a7
gltknbtn <gltknbtn@hotmail.com>
2016-02-02 07:23:46 +0200

    initial commit

---
+++ src/com/androidhive/dashboard/AndroidDashboardDesignActivity.java
@@ -0,0 +1,67 @@
+package com.androidhive.dashboard;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidhive.dashboard.R;
+
+public class AndroidDashboardDesignActivity extends Activity {
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard_layout);
+        
+        /**
+         * Creating all buttons instances
+         * */
+        // Dashboard News feed button
+        Button btn_newsfeed = (Button) findViewById(R.id.btn_news_feed);
+        
+        // Dashboard Friends button
+        Button btn_friends = (Button) findViewById(R.id.btn_friends);
+        
+        // Dashboard Messages button
+        Button btn_messages = (Button) findViewById(R.id.btn_messages);
+        
+        /**
+         * Handling all button click events
+         * */
+        
+        // Listening to News Feed button click
+        btn_newsfeed.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+				// Launching News Feed Screen
+				Intent i = new Intent(getApplicationContext(), NewsFeedActivity.class);
+				startActivity(i);
+			}
+		});
+        
+       // Listening Friends button click
+        btn_friends.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+				// Launching News Feed Screen
+				Intent i = new Intent(getApplicationContext(), FriendsActivity.class);
+				startActivity(i);
+			}
+		});
+        
+        // Listening Messages button click
+        btn_messages.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+				// Launching News Feed Screen
+				Intent i = new Intent(getApplicationContext(), MessagesActivity.class);
+				startActivity(i);
+			}
+		});
+        
+    }
+}
